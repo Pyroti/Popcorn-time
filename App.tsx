64b7730ebea88414cf795770c90f15add89d0d1f
User <user@MacBook-Pro-User.local>
2022-06-30 13:59:52 +0300

    init project

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
